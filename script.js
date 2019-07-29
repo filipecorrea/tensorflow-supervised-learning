53cdc60015c9043fe2528a319cba9276d2a2243a
Filipe Corrêa <filipecorrea@br.ibm.com>
2019-07-29 10:50:32 -0300

    Define the model architecture

--- script.js
+++ script.js
@@ -3,24 +3,24 @@
  * and cleaned of missing data.
  */
 async function getData() {
-  const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');  
-  const carsData = await carsDataReq.json();  
+  const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json')
+  const carsData = await carsDataReq.json()
   const cleaned = carsData.map(car => ({
     mpg: car.Miles_per_Gallon,
     horsepower: car.Horsepower,
   }))
-  .filter(car => (car.mpg != null && car.horsepower != null));
+  .filter(car => (car.mpg != null && car.horsepower != null))
   
-  return cleaned;
+  return cleaned
 }
 
 async function run() {
   // Load and plot the original input data that we are going to train on.
-  const data = await getData();
+  const data = await getData()
   const values = data.map(d => ({
     x: d.horsepower,
     y: d.mpg,
-  }));
+  }))
 
   tfvis.render.scatterplot(
     {name: 'Horsepower v MPG'},
@@ -30,9 +30,25 @@ async function run() {
       yLabel: 'MPG',
       height: 300
     }
-  );
+  )
 
   // More code will be added below
+  // Create the model
+  const model = createModel()
+  tfvis.show.modelSummary({name: 'Model Summary'}, model)
 }
 
-document.addEventListener('DOMContentLoaded', run);
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential()
+  
+  // Add a single hidden layer
+  model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}))
+  
+  // Add an output layer
+  model.add(tf.layers.dense({units: 1, useBias: true}))
+
+  return model
+}
+
+document.addEventListener('DOMContentLoaded', run)
